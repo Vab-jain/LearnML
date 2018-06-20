512acbf028686f8f79c6cc1b6ac55f9def3d77ca
Vab-jain <vabsweb@gmail.com>
2018-06-20 14:36:37 +0530

    adding movie recommender

---
+++ basic_movie_recommender.py
@@ -0,0 +1,63 @@
+##### Movie recommender system #####
+'''
+	Recommender systems are of two types
+	1) Content based: What YOU previously liked
+	2) Collaborative: Based on what others previously liked
+
+	LightFM library is great for getting started
+		with recommendation algorithms.
+
+'''
+
+import numpy as np
+from lightfm.datasets import fetch_movielens
+from lightfm import LightFM
+
+# fetch data and format it
+
+data = fetch_movielens(min_rating = 4.0)
+
+# printtraining and testing data
+print(repr(data['train']))
+print(repr(data['test']))
+
+# create model
+model = LightFM(loss = 'warp')
+'''
+ 	warp = Weighted Approximate Rank Pairwise
+	-> Uses Gradient Descent method to find weights
+	-> Hybrid system = content + collaborative
+'''
+# train model
+model.fit(data['train'],epochs=30, num_threads=2)
+
+#recommender
+def sample_recommendation(model, data, user_ids):
+
+	# number of users and movies for each user we input
+	n_users, n_items = data['train'].shape
+
+	# generate recommendations for each user we input
+	for user_id in user_ids:
+
+		#movies they already like
+		known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
+
+		#movies our model predicts they will like
+		scores = model.predict(user_id, np.arange(n_items))
+		#rank them in order of most likelyhood
+		top_items = data['item_labels'][np.argsort(-scores)]
+
+		#printout results
+		print("User %s"% user_id)
+		print("			Known Positivies:")
+
+		for x in known_positives[:3]:
+			print("			%s"%x)
+
+		print("			Recommended:")
+
+		for x in top_items[:3]:
+			print("			%s"%x)
+
+sample_recommendation(model, data,[3,25,450])
